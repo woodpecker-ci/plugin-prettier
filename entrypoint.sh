019b0ce0a84abde64431af63d9f8e9b0b9c42494
qwerty287 <80460567+qwerty287@users.noreply.github.com>
2024-01-15 20:11:45 +0100

    Allow to specify version (#18)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #! /bin/sh
 
+if [ -n "$PLUGIN_VERSION" ] && [ "$PLUGIN_VERSION" != "$(prettier --version)" ]; then
+  npm install --global "prettier@$PLUGIN_VERSION"
+fi
+
 PRETTIER_PATH="."
 
 if [ -n "$PLUGIN_PATH" ]; then
